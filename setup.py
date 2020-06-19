8f08e3d90c4ee505f8e0a61a825456bb88821e72
Nycholas de Oliveira e Oliveira <nycholas@gmail.com>
2020-06-19 10:21:51 -0300

    Version 1.0.0

--- setup.py
+++ setup.py
@@ -28,11 +28,9 @@
 # POSSIBILITY OF SUCH DAMAGE.
 from setuptools import setup
 
-import flask_jsonrpc
-
 setup(
     name='Flask-JSONRPC',
-    version=flask_jsonrpc.__version__,
+    version='1.0.0',
     url='https://github.com/cenobites/flask-jsonrpc',
     license='New BSD License',
     author='Nycholas de Oliveira e Oliveira',
