39019e7c8bc8d5dc1666a491a70c07d05fd38d0d
Nycholas de Oliveira e Oliveira <nycholas@gmail.com>
2022-09-14 11:05:29 -0300

    Bump typing-inspect from 0.7.1 to 0.8.0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setuptools.setup(
         'typing>=3.7.4;python_version<"3.5"',
         'typing_extensions>=4.3.0;python_version>"3.6"',
         'typing_extensions>=3.7.4,<3.10;python_version<="3.6"',
-        'typing_inspect==0.7.1',
+        'typing_inspect==0.8.0',
     ],
     extras_require={
         'async': ['Flask[async]>=1.0.0,<3.0'],
