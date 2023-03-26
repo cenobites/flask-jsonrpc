990918fb95f0901102c3770ebf3e7f76e64bf18d
Nycholas de Oliveira e Oliveira <nycholas@gmail.com>
2023-03-26 10:44:00 -0300

    Add tests to all Python versions supported by the project
    Add unit tests and integration tests for each Python version supported
    by the project.
    
    The integration tests are essential for the next steps, which will be changes
    to Browser API.
    
    *Note:*  The work of saying good bye to Python 3.6 starts here.
    
    See also: #394

--- bin/docker-compose-test.sh
+++ bin/docker-compose-test.sh
@@ -7,9 +7,6 @@ DOCKER_COMPOSE_FILE_PATH=../${DOCKER_COMPOSE_FILE_NAME}
 docker-compose -f ${DOCKER_COMPOSE_FILE_PATH} -p ci build --build-arg VERSION=$(date +%s)
 docker-compose -f ${DOCKER_COMPOSE_FILE_PATH} -p ci --compatibility up -d
 
-DOCKER_WAIT_FOR_PY36=$(docker wait ci_python3.6_1)
-docker logs ci_python3.6_1
-
 DOCKER_WAIT_FOR_PY37=$(docker wait ci_python3.7_1)
 docker logs ci_python3.7_1
 
@@ -24,7 +21,6 @@ docker logs ci_python3.10_1
 
 docker-compose -f ${DOCKER_COMPOSE_FILE_PATH} -p ci down --remove-orphans
 
-if [ ${DOCKER_WAIT_FOR_PY36} -ne 0 ]; then echo "Test to Python 3.6 failed"; exit 1; fi
 if [ ${DOCKER_WAIT_FOR_PY37} -ne 0 ]; then echo "Test to Python 3.7 failed"; exit 1; fi
 if [ ${DOCKER_WAIT_FOR_PY38} -ne 0 ]; then echo "Test to Python 3.8 failed"; exit 1; fi
 if [ ${DOCKER_WAIT_FOR_PY39} -ne 0 ]; then echo "Test to Python 3.9 failed"; exit 1; fi
