f11d87573d28d0a6ef4eec6d78788641561afe16
Nycholas de Oliveira e Oliveira <nycholas@gmail.com>
2024-09-11 17:30:39 -0300

    Refactoring backend to compile Python modules to C extensions using Mypyc

--- setup.py
+++ setup.py
@@ -27,16 +27,32 @@
 # POSSIBILITY OF SUCH DAMAGE.
 import os
 import sys
+import typing as t
 import pathlib
 
 import setuptools
 
-setup_attrs = {}
 
-if 'bdist_wheel' in sys.argv and os.environ.get('MYPYC_ENABLE', 'False').lower() in ('true', 't', '1'):
+def find_python_files(path: pathlib.Path) -> t.List[pathlib.Path]:
+    return path.rglob('*.py')
+
+
+USE_MYPYC = os.getenv('MYPYC_ENABLE', 'False').lower() in ('true', 't', '1')
+if len(sys.argv) > 1 and sys.argv[1] == '--use-mypyc':
+    USE_MYPYC = True
+
+setup_attrs = {'name': 'Flask-JSONRPC', 'packages': setuptools.find_packages()}
+
+if USE_MYPYC:
     from mypyc.build import mypycify  # pylint: disable=E0611
 
     project_dir = pathlib.Path(__file__).resolve().parent
+
+    blocklist = []
+
+    discovered = []
+    discovered.extend(find_python_files(project_dir / 'src' / 'flask_jsonrpc'))
+
     ext_modules = [
         '--config-file',
         str(project_dir / 'pyproject.toml'),
@@ -50,8 +66,11 @@ if 'bdist_wheel' in sys.argv and os.environ.get('MYPYC_ENABLE', 'False').lower()
         '--disable-error-code',
         'misc',
     ]
-    ext_modules.extend([str(file) for file in (project_dir / 'src' / 'flask_jsonrpc').rglob('*.py')])
-    setup_attrs['ext_modules'] = mypycify(ext_modules)
+    ext_modules.extend([str(p) for p in discovered if p.relative_to(project_dir).as_posix() not in blocklist])
+
+    opt_level = os.getenv('MYPYC_OPT_LEVEL', '3')
+    setup_attrs['requires'] = ['mypy']
+    setup_attrs['ext_modules'] = mypycify(ext_modules, opt_level=opt_level, verbose=True)
     setup_attrs['package_data'] = {'flask_jsonrpc.contrib.browse': ['static*', 'templates*']}
 
 setuptools.setup(**setup_attrs)
