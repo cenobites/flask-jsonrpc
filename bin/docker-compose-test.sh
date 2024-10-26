1c994de4e3fe61a07cc8aa06490d374c01185faa
Alex Ted <alextedx@gmail.com>
2024-10-27 01:12:37 +0300

    build: added new version of Python interpreter
    # Conflicts:
    #	.github/workflows/on_update.yml
    #	.github/workflows/pre_release.yml
    #	.github/workflows/tests.yml
    #	Dockerfile.it
    #	Dockerfile.local
    #	examples/javascript/tox.ini
    #	examples/minimal-async/tox.ini
    #	examples/minimal/tox.ini
    #	examples/modular/tox.ini
    #	examples/multiplesite/tox.ini
    #	examples/openrpc/tox.ini
    #	requirements/typing.txt
    #	src/flask_jsonrpc/contrib/browse/static/js/apps/browse/services.js

--- bin/docker-compose-test.sh
+++ bin/docker-compose-test.sh
@@ -35,9 +35,17 @@ DOCKER_WAIT_FOR_PY311=$?
 )
 DOCKER_WAIT_FOR_PY312=$?
 
+(
+    set -e
+    docker wait ci-python3.13-1
+    docker logs ci-python3.13-1
+)
+DOCKER_WAIT_FOR_PY313=$?
+
 docker compose -f ${DOCKER_COMPOSE_FILE_PATH} -p ci down --remove-orphans
 
 if [ ${DOCKER_WAIT_FOR_PY39} -ne 0 ]; then echo "Test to Python 3.9 failed"; exit 1; fi
 if [ ${DOCKER_WAIT_FOR_PY310} -ne 0 ]; then echo "Test to Python 3.10 failed"; exit 1; fi
 if [ ${DOCKER_WAIT_FOR_PY311} -ne 0 ]; then echo "Test to Python 3.11 failed"; exit 1; fi
 if [ ${DOCKER_WAIT_FOR_PY312} -ne 0 ]; then echo "Test to Python 3.12 failed"; exit 1; fi
+if [ ${DOCKER_WAIT_FOR_PY313} -ne 0 ]; then echo "Test to Python 3.13 failed"; exit 1; fi
